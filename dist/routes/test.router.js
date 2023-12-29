7bdd7a867948e8d957385f3c64462c0a718946bf
vinayak-naik <vinayaknaik2021@gmail.com>
2023-12-29 18:58:45 +0530

    update23

---
+++ dist/routes/test.router.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const testRouter = (0, express_1.Router)();
+testRouter.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Server is running",
+    });
+});
+exports.default = testRouter;
--- src/routes/test.router.ts
+++ src/routes/test.router.ts
@@ -3,7 +3,10 @@ import { Request, Response, Router } from "express";
 const testRouter = Router();
 
 testRouter.get("/", (req: Request, res: Response) => {
-  res.send("Server is running");
+  res.status(200).json({
+    success: true,
+    message: "Server is running --Vinayak",
+  });
 });
 
 export default testRouter;
