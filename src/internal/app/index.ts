29e7ab42c62aa563446b3b9b2f9d39ca016d2605
vinayak-naik <vinayaknaik2021@gmail.com>
2023-12-29 18:46:04 +0530

    update21

--- src/internal/app/index.ts
+++ src/internal/app/index.ts
@@ -1,19 +1,19 @@
 import express, { Request, Response } from "express";
 import { injectable } from "tsyringe";
 import allRoutes from "../../routes";
-import errorHandler from "../../middleware/error/api-error-handler";
+// import errorHandler from "../../middleware/error/api-error-handler";
 
 @injectable()
 class App {
   setup = () => {
     const app = express();
     app.use(express.json());
-    app.get("/", (req: Request, res: Response) => {
+    app.get("/test", (req: Request, res: Response) => {
       res.send("This is test api!");
     });
     app.use(allRoutes);
 
-    app.use(errorHandler);
+    // app.use(errorHandler);
     return app;
   };
 }
